39cad86f206c97549d787372db4f27dae05e72e3
Adelaide <88384474+allytyson16@users.noreply.github.com>
2022-02-21 12:55:54 +0000

    Add files via upload

---
+++ method-ex3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace method_ex3
+{
+    class Program
+    {
+        /*
+         * 
+         *  Create a method named Sum() that accepts any number of
+            integer parameters and displays their sum. Write a Main()
+            method that demonstrates the Sum() method works correctly
+            when passed one, three, or five integers, or an array of ten
+            integers
+         * 
+         * 
+         * */
+        static void Main(string[] args)
+        {
+            Sum(5);
+            Sum(5, 10);
+            Sum(5, 10, 15, 20, 25);
+            int[] myArray = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+            Sum(myArray);
+        }
+        public static void Sum(params int[] myArray )
+        {
+            int sum = 0;
+            for(int i = 0; i < myArray.Length; i++)
+            {
+                sum += myArray[i];
+            }
+            Console.WriteLine("The sum is {0}", sum);
+        }
+    }
+}
